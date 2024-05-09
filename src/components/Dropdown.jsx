ec9a958ac8dace20d5078246993436b208addfd4
harshit gururani <harshit10gururani7@gmail.com>
2024-05-09 12:57:56 +0530

    update dropdown menu

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,8 +1,10 @@
 import { MdKeyboardArrowDown, MdKeyboardArrowUp } from "react-icons/md";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Dropdown = () => {
   const [dropMenu, setDropMenu] = useState(false);
+  const navigate = useNavigate();
   return (
     <div className="w-[120px]" onClick={() => setDropMenu(!dropMenu)}>
       <div className="dropdown w-full relative text-slate-700 z-0 ">
@@ -21,21 +23,46 @@ const Dropdown = () => {
           } transition-opacity w-full top-8 opacity-85  `}
         >
           <ul
-            className="bg-slate-900 text-white rounded-md overflow-hidden"
+            className="bg-slate-900 text-white rounded-md overflow-hidden "
             onClick={() => setDropMenu(!dropMenu)}
           >
-            <li className="cursor-pointer p-3 hover:bg-slate-950">
-              <a href="#sem1">Semster 1</a>
-            </li>
-            <li className="cursor-pointer p-3 hover:bg-slate-950">
-              <a href="#sem2">Semster 2</a>
-            </li>
-            <li className="cursor-pointer p-3 hover:bg-slate-950">
-              <a href="#sem3">Semster 3</a>
-            </li>
-            <li className="cursor-pointer p-3 hover:bg-slate-950">
-              <a href="#sem4">Semster 4</a>
-            </li>
+            <a
+              href="#sem1"
+              className="scroll-smooth"
+              onClick={() => navigate("/#sem1")}
+            >
+              <li className="cursor-pointer p-3 hover:bg-slate-950">
+                Semster 1
+              </li>
+            </a>
+
+            <a
+              href="#sem2"
+              className="scroll-smooth"
+              onClick={() => navigate("/#sem1")}
+            >
+              <li className="cursor-pointer p-3 hover:bg-slate-950">
+                Semster 2
+              </li>
+            </a>
+            <a
+              href="#sem3"
+              className="scroll-smooth"
+              onClick={() => navigate("/#sem1")}
+            >
+              <li className="cursor-pointer p-3 hover:bg-slate-950">
+                Semster 3
+              </li>
+            </a>
+            <a
+              href="#sem4"
+              className="scroll-smooth"
+              onClick={() => navigate("/#sem1")}
+            >
+              <li className="cursor-pointer p-3 hover:bg-slate-950">
+                Semster 4
+              </li>
+            </a>
           </ul>
         </div>
       </div>
